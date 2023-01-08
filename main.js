069e28fee9a8130b8904ab46826cbf418999f25e
Camille-BAE <camille.jbae@gmail.com>
2023-01-09 00:20:07 +0100

    sorry sensei

---
+++ main.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('button');
+button.addEventListener('click', function () {
+  alert (`Bonjour Sensei. Je suis désolée mais le JS n'est pas encore terminé...`)
+});
+
