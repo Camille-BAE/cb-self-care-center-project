24f05d245a36770c24a0074498812909b2b2dd49
Camille-BAE <camille.jbae@gmail.com>
2023-01-09 09:19:58 +0100

    second try

--- main.js
+++ main.js
@@ -3,3 +3,37 @@ button.addEventListener('click', function () {
   alert (`Bonjour Sensei. Je suis désolée mais le JS n'est pas encore terminé...`)
 });
 
+// const button = document.querySelector('button');
+// const affirmCitation = document.querySelector('#affirmCitation');
+// const mantraCitation = document.querySelector('#mantraCitation');
+
+// affirmCitation.addEventListener ('click', function () {
+//  let affirmMessage = document.getByClassName('.affirmMessage');
+//  => utiliser une condition avec un Maths.random()
+// });
+//
+// mantraCitation.addEventListener ('click', function () {
+//  let mantraMessage = document.getByClassName('.mantraMessage');
+//  => utiliser une condition avec un Maths.random()
+// });
+//
+// button.addEventListener ('click', function () {
+//  
+//  if() {
+//    
+//  }else {
+//    
+//  };
+//});
+
+
+
+////////////////////////////////////////////////////////////////////////////
+//ce que je veux :
+// 1) je veux que lorsque je sélectionne mantra et que je clique sur le bouton, qu'un message de mantra s'affiche de manière aléatoire et fait disparaître l'image.
+// 2) si je clique de nouveau sur le bouton, un autre message aléatoire de mantra apparaît et vient remplacer l'ancien message.
+// 3) si je décice de changer et de sélectionner affirmation, et que je clique sur le bouton, un message de affirmation doit apparaître et remplacer le message de mantra.
+// Et Vice-Versa
+// 1) je veux que lorsque je séléctionne affirmation et que je clique sur le bouton, qu'un message de affirmation s'affiche de manière aléatoire et fait disparaître l'image.
+// 2) si je clique de nouveau sur le bouton, un autre message aléatoire de affirmation apparaît et vient remplacer l'ancien message.
+// 3) si je décide de changer et de sélectionner mantra et que je clique sur le bouton, un message de mantra doit apparaître et remplacer le message de affirmation.
